1b51893c186d01a538d993687a3f9f1415864315
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-06-30 16:02:22 -0500

    update

--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -13,7 +13,7 @@ function App() {
     setCities(oldCities => oldCities.filter(c => c.id !== id));
   }
 
-  function onSearch(ciudad) {
+  /*function onSearch(ciudad) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
@@ -36,7 +36,35 @@ function App() {
           alert("City not found");
         }
       });
+  }*/
+
+  async function onSearch(ciudad) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+    const recurso = await response.json();
+    try{
+      if(recurso.main !== undefined){
+        const ciudad = {
+          min: Math.round(recurso.main.temp_min),
+          max: Math.round(recurso.main.temp_max),
+          img: recurso.weather[0].icon,
+          id: recurso.id,
+          wind: recurso.wind.speed,
+          temp: recurso.main.temp,
+          name: recurso.name,
+          weather: recurso.weather[0].main,
+          clouds: recurso.clouds.all,
+          latitud: recurso.coord.lat,
+          longitud: recurso.coord.lon
+        };
+        setCities(oldCities => [...oldCities, ciudad]);
+      }
+    } catch(error){
+        alert("City not found " + error);
+    }
   }
+
+
+
   function onFilter(ciudadId) {
     let ciudad = cities.find(ciudad => ciudad.id === parseInt(ciudadId));
     return ciudad;
